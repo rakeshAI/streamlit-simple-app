03ef3ccf28ff16bd7907e5134f6e90c9865a6e48
unknown <rakeshkumarcool2003@gmail.com>
2020-06-30 09:29:26 +0900

    [mod] page updated

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
 import streamlit as st
+#Text
+st.title("I Love You Bangaram")
 
-st.header("streamlit header")
+#Images
+from PIL import Image
+img = Image.open("love.jpg")
+st.image(img,width=500,caption="smiling love")
+
+
+
+
+#Header
+st.title("streamlit tutorial")
+st.header("This is a header")
+st.subheader("This is a subheader")
+
+#Text
+st.text("Hello Streamlit")
+
+#Markdown
+st.markdown("### This is Markdown")
+
+
+#Error/colorful
+st.success("Successful")
+
+st.info("Information!")
+
+st.warning("This is a warning")
+
+st.error("This is an error")
+
+st.exception("NameError('name three not defined')")
+
+
+#Get Info about python
+st.help(range)
+
+#writing text
+st.write("Text with write")
+
+st.write(range(10))
+
+
